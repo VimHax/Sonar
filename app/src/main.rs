fea82160940086d2683587c991b058700a237fc2
VimHax <vimukthi.weerabahu@gmail.com>
2024-03-28 17:41:39 +0530

    Update `AppState` code

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use replace_with::replace_with_or_abort;
+use egui::TextEdit;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init();
@@ -20,46 +20,26 @@ struct Auth {
 }
 
 enum AppState {
-    LoggedOut { auth: Auth },
-    LoggedIn { auth: Auth },
+    LoggedOut,
+    VerifyingAuth,
+    LoggedIn,
 }
 
-impl AppState {
-    pub fn auth(&mut self) -> &mut Auth {
-        match self {
-            AppState::LoggedOut { auth } => auth,
-            AppState::LoggedIn { auth } => auth,
-        }
-    }
-
-    pub fn logged_in(&self) -> bool {
-        match self {
-            AppState::LoggedOut { .. } => false,
-            AppState::LoggedIn { .. } => true,
-        }
-    }
-
-    pub fn toggle(&mut self) {
-        replace_with_or_abort(self, |state| match state {
-            AppState::LoggedOut { auth } => AppState::LoggedIn { auth },
-            AppState::LoggedIn { auth } => AppState::LoggedOut { auth },
-        });
-    }
-}
+impl AppState {}
 
 struct App {
+    auth: Auth,
     state: AppState,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            state: AppState::LoggedOut {
-                auth: Auth {
-                    host: String::new(),
-                    password: String::new(),
-                },
+            auth: Auth {
+                host: String::new(),
+                password: String::new(),
             },
+            state: AppState::LoggedOut,
         }
     }
 }
@@ -67,51 +47,49 @@ impl Default for App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::top("auth").show(ctx, |ui| {
-            let logged_out = !self.state.logged_in();
-            ui.add_enabled_ui(logged_out, |ui| {
+            ui.add_space(4.0);
+
+            ui.add_enabled_ui(matches!(self.state, AppState::LoggedOut), |ui| {
                 egui::Grid::new("auth_grid")
                     .num_columns(2)
-                    .spacing([40.0, 4.0])
+                    .spacing([4.0, 4.0])
                     .show(ui, |ui| {
                         ui.label("Host: ");
-                        ui.text_edit_singleline(&mut self.state.auth().host);
+                        ui.text_edit_singleline(&mut self.auth.host);
                         ui.end_row();
                         ui.label("Password: ");
-                        ui.text_edit_singleline(&mut self.state.auth().password);
+                        ui.add(TextEdit::singleline(&mut self.auth.password).password(true));
                         ui.end_row();
                     });
             });
 
-            if ui
+            ui.add_space(4.0);
+
+            if matches!(self.state, AppState::VerifyingAuth) {
+                ui.horizontal(|ui| {
+                    ui.label("Verifying details...");
+                    ui.spinner();
+                });
+            } else if ui
                 .button(match self.state {
-                    AppState::LoggedOut { .. } => "Login",
-                    AppState::LoggedIn { .. } => "Logout",
+                    AppState::LoggedOut => "Login",
+                    AppState::LoggedIn => "Logout",
+                    AppState::VerifyingAuth => unreachable!(),
                 })
                 .clicked()
             {
-                self.state.toggle();
-            };
+                self.state = match self.state {
+                    AppState::LoggedOut => AppState::VerifyingAuth,
+                    AppState::VerifyingAuth => AppState::LoggedIn,
+                    AppState::LoggedIn => AppState::LoggedOut,
+                }
+            }
+
+            ui.add_space(4.0);
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Hello World!");
         });
-        // egui::CentralPanel::default().show(ctx, |ui| {
-        //     ui.heading("My egui Application");
-        //     ui.horizontal(|ui| {
-        //         let name_label = ui.label("Your name: ");
-        //         ui.text_edit_singleline(&mut self.name)
-        //             .labelled_by(name_label.id);
-        //     });
-        //     ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-        //     if ui.button("Increment").clicked() {
-        //         self.age += 1;
-        //     }
-        //     ui.label(format!("Hello '{}', age {}", self.name, self.age));
-
-        //     // ui.image(egui::include_image!(
-        //     //     "../../../crates/egui/assets/ferris.png"
-        //     // ));
-        // });
     }
 }

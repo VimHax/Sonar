7773c7a361bb5f127781236a73544520e16dbc4a
VimHax <vimukthi.weerabahu@gmail.com>
2024-04-01 03:30:28 +0530

    Add initial page

---
+++ app/lib/page/initial/page.dart
@@ -0,0 +1,47 @@
+import 'package:app/util/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: FittedBox(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  RichText(
+                      text: TextSpan(
+                          style: GoogleFonts.robotoMono(
+                              textStyle: const TextStyle(
+                                  color: BrandColors.red,
+                                  fontSize: 15,
+                                  letterSpacing: 10,
+                                  fontWeight: FontWeight.w500)),
+                          children: const [
+                        TextSpan(text: "V1.0.0 "),
+                        TextSpan(
+                            text: "-",
+                            style: TextStyle(color: BrandColors.dimBlue)),
+                        TextSpan(text: " BY VIMHAX")
+                      ])),
+                  Text("SONAR",
+                      style: GoogleFonts.montserrat(
+                          textStyle: const TextStyle(
+                              color: BrandColors.white,
+                              fontSize: 100,
+                              letterSpacing: 50,
+                              fontWeight: FontWeight.w600))),
+                  const SizedBox(height: 10),
+                ]),
+          ),
+        )
+      ],
+    );
+  }
+}
